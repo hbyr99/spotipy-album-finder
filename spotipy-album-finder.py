66ec181cf9933dcb626d27e577fa4e4c0dd4979c
Henry Baldacci <henry.louis.980@gmail.com>
2021-06-30 17:02:48 +0000

    Added database functionality

--- spotipy-album-finder.py
+++ spotipy-album-finder.py
@@ -2,10 +2,15 @@
 import requests
 import spotipy
 import json
+import pandas as pd
+from sqlalchemy import create_engine
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
 
 # Insert your API ID and Secret key here
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = '080d31685d824fd8bcc84b364770fd59'
+CLIENT_SECRET = '28810ffc910745e1869f5a243f92e23f'
 
 
 # POST
@@ -23,19 +28,33 @@ access_token = auth_response_data['access_token']
 
 headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
 
-# base URL of all Spotify API endpoints
-BASE_URL = 'https://api.spotify.com/v1/'
-
-# Request user for artist ID
+# Request user for artist ID and number of albums to pull
 print("Welcome to album finder!")
 print("Insert the artist ID: ")
 artist_id = input()
 
+print("How many albums would you like to see? ")
+limit = int(input())
+
 # GET request for albums
 albums = requests.get(BASE_URL + 'artists/' + artist_id + '/albums',
                       headers=headers,
-                      params={'include_groups': 'album', 'limit': 10})
+                      params={'include_groups': 'album', 'limit': limit})
 album_list = albums.json()
 
 for album in album_list['items']:
     print(album['name'], ' --- ', album['release_date'])
+
+# creating data frame to add data to
+col_names = ['album', 'release_date']
+albums_df = pd.DataFrame(columns = col_names)
+for album in album_list['items']:
+    albums_df.loc[len(albums_df.index)] = [
+        album['name'], album['release_date']
+    ]
+    
+print('I got this far!')
+print(albums_df)
+
+engine = create_engine('mysql://root:codio@localhost/album_finder')
+albums_df.to_sql('albums', con=engine, if_exists='replace', index=False)
